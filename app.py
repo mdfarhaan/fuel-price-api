25a01594f79de9ccb9c1c4b2896197a0011e0465
mdfarhaan <farhaanthestar@gmail.com>
2021-12-29 14:22:39 +0530

    :sparkles: Get latest price

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, jsonify
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup,NavigableString, Comment
 
 app = Flask(__name__)
 
@@ -11,27 +11,37 @@ def home():
 @app.route('/<district>')
 def fuelPrice(district):
     price = {"District": district}
-    try:
+    # try:
         #Get petrol price
-        petrol_url = "https://www.goodreturns.in/petrol-price-in-" + district + ".html"
-        petrol_response = requests.get(petrol_url)
-        petrol_src = petrol_response.content
-        petrol_soup = BeautifulSoup(petrol_src, "html.parser")
-        petrol_tr = petrol_soup.find("tr", {"class":"odd_row"})
-        petrol_td = petrol_tr.find_all("td")[1]
-        
-        #Get diesel price
-        diesel_url = "https://www.goodreturns.in/diesel-price-in-" + district + ".html"
-        diesel_response = requests.get(diesel_url)
-        diesel_src = diesel_response.content
-        diesel_soup = BeautifulSoup(diesel_src, "html.parser")
-        diesel_tr = diesel_soup.find("tr", {"class":"odd_row"})
-        diesel_td = diesel_tr.find_all("td")[1]
+    petrol_url = "https://www.goodreturns.in/petrol-price-in-" + district + ".html"
+    petrol_response = requests.get(petrol_url)
+    petrol_src = petrol_response.content
+    petrol_soup = BeautifulSoup(petrol_src, "html.parser")
+    petrol_block = str(petrol_soup.find("div", {"class":"fuel-block-details"})) 
+    petrol_price = ""   
+    for i in range(len((petrol_block))) :
+        if petrol_block[i].isdigit():
+            petrol_price += petrol_block[i]
+        elif petrol_block[i] == "." :
+            petrol_price += petrol_block[i]
+    
+    #Get diesel price
+    diesel_url = "https://www.goodreturns.in/diesel-price-in-" + district + ".html"
+    diesel_response = requests.get(diesel_url)
+    diesel_src = diesel_response.content
+    diesel_soup = BeautifulSoup(diesel_src, "html.parser")
+    diesel_block = str(diesel_soup.find("div", {"class":"fuel-block-details"})) 
+    diesel_price = ""   
+    for i in range(len((diesel_block))) :
+        if diesel_block[i].isdigit():
+            diesel_price += diesel_block[i]
+        elif diesel_block[i] == "." :
+            diesel_price += diesel_block[i]
 
-        price["Petrol"] = petrol_td.text[2:]
-        price["Diesel"] = diesel_td.text[2:]
-    except :
-        price = {"ERROR 404": "Not Found"}
+    price["Petrol"] = petrol_price
+    price["Diesel"] = diesel_price
+    # except :
+    #     price = {"ERROR 404": "Not Found"}
     
     return jsonify(price)
 
